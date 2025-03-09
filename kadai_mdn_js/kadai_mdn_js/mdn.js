a7df60afddd5bd39853700945b1064826cd4c683
masamin1810 <satomasamin1810@icloud.com>
2025-03-09 23:19:48 +0900

    Add files via upload

---
+++ kadai_mdn_js/kadai_mdn_js/mdn.js
@@ -0,0 +1,6 @@
+const now = new Date('2024-10-12'); 
+const year = now.getFullYear(); 
+const month = now.getMonth() + 1; 
+const date = now.getDate(); 
+const output = `${year}年${month}月${date}日`; 
+console.log(output); 
